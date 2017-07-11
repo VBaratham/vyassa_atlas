378c16618a0dea3856f32f0093fbac70ca951dd8
Vyassa Baratham <vbaratham@berkeley.edu>
2017-07-11 11:04:46 -0700

    proper normalization in merge; fitting for symmetric dijet (2d) dist'n

--- fit_bkg.py
+++ fit_bkg.py
@@ -6,25 +6,56 @@ from ROOT import TFile, TF2, TF1
 Playing around with fitting to the merged bkg file
 """
 
+# mjj_range = (800, 3000) # dijet falling spectrum
+# mavg_range = (35, 110) # average m_j falling spectrum
+
+mjj_range = (800, 2500) # dijet falling spectrum
+mavg_range = (35, 90) # average m_j falling spectrum
 
 def main():
     f = TFile(sys.argv[1])
-    # jetmass = f.Get("jetmass")
-    # histo = jetmass.Project3D("yz")
-    dijet = f.Get("dijetmass")
-    fit_fcn = TF1(
-        "dijet", "[0]*(1-x/13000)^[1]*(x/13000)^([2] + [3]*log(x/13000))",
-        dijet.GetXaxis().GetXmin(),
-        dijet.GetXaxis().GetXmax(),
+
+
+    # # jetmass = f.Get("jetmass")
+    # # histo = jetmass.Project3D("yz")
+    # dijet = f.Get("mjj_avg")
+    # fit_fcn = TF1(
+    #     "dijet", "[0]*(1-x/13000)^[1]*(x/13000)^([2] + [3]*log(x/13000))",
+    #     # dijet.GetXaxis().GetXmin(),
+    #     # dijet.GetXaxis().GetXmax(),
+    #     *mjj_range
+    # )
+    # fit_fcn.SetNpx(jetmass.GetNbinsX())
+    # fit_fcn.SetParameter(0, 100000)
+    # fit_fcn.SetParameter(1, 15)
+    # fit_fcn.SetParameter(2, 15)
+    # fit_fcn.SetParameter(3, 2)
+    
+    # dijet.Fit(fit_fcn, "", "", *mjj_range)
+
+    
+    
+    jetmass = f.Get("jetmass_avg")
+    jetmass.SetAxisRange(*(mavg_range+('x',)))
+    jetmass.SetAxisRange(*(mjj_range+('y',)))
+    
+    fit_fcn = TF2(
+        "mjj_avg_fcn", "[0]*(1-x/13000)^[1]*(x/13000)^([2] + [3]*log(x/13000))*" + 
+        "(1-y/13000)^[4]*(y/13000)^([5] + [6]*log(y/13000))",
+        *(mavg_range+mjj_range)
     )
-    
-    fit_fcn.SetParameter(0, 100000)
+    # fit_fcn.SetNpx(jetmass.GetNbinsX())
+    # fit_fcn.SetNpy(jetmass.GetNbinsY())
+    fit_fcn.SetParameter(0, 5000)
     fit_fcn.SetParameter(1, 15)
-    fit_fcn.SetParameter(2, 15)
-    fit_fcn.SetParameter(3, 2)
-    
-    dijet.Fit(fit_fcn)
-    
+    fit_fcn.SetParameter(2, -4)
+    fit_fcn.SetParameter(3, -1)
+    fit_fcn.SetParameter(4, 15)
+    fit_fcn.SetParameter(5, -4)
+    fit_fcn.SetParameter(6, -1)
+
+    jetmass.Fit(fit_fcn)
+        
 
     import pdb; pdb.set_trace()
 
--- merge_bkg.py
+++ merge_bkg.py
@@ -1,15 +1,19 @@
-from ROOT import TH3D,TH1D, TFile, TLorentzVector
-import sys, os
-from operator import itemgetter
+from ROOT import TH3D, TH2D, TH1D, TFile, TLorentzVector
+import sys, os, argparse
+from operator import itemgetter, mul
 
 """
-Merge outfiles from grid jobs into one final histogram, including proper event and sample (TODO) weighting
+Merge outfiles from grid jobs into one final histogram, including proper event and sample weighting
 """
+
+MASS_DIFF_CUTOFF = 25
 
 def iter_outfiles(dirname):
     for fn in os.listdir(dirname):
         if fn.endswith(".root"):
-            yield TFile(os.path.join(dirname, fn), "READ")
+            f = TFile(os.path.join(dirname, fn), "READ")
+            yield f
+            f.Close()
 
 def masses(evt):
     sort_order = [i[0] for i in sorted(enumerate(evt.JetPt), key=itemgetter(1))]
@@ -22,31 +26,57 @@ def masses(evt):
     subl_vector.SetPtEtaPhiM(evt.JetPt[j], evt.JetEta[j], evt.JetPhi[j], evt.JetM[j])
     return evt.JetM[i], evt.JetM[j], (lead_vector + subl_vector).Mag(), evt.JetPt[i], evt.JetPt[j]
 
-def main():
+def main(args):
+    lumi = 40e6 # 6 orders of magnitude between femto- and nano-
+    normalization = {
+        # i: xsec (nb) * filt_eff * kfactor / nevents * lumi
+        7: .016215 * 3.9216e-4 * 1 / 1770193 * lumi,
+        6: .25753 * 9.4106e-4 * 1 / 1893389 * lumi,
+        5: 4.5535 * 9.2196e-4 * 1 / 7977567 * lumi,
+        4: 254.63 * 5.3015e-4 * 1 / 7975217 * lumi,
+        3: 26454 * 3.1956e-4 * 1 / 7349799 * lumi,
+    }
 
-    m1 = TH1D("jet1m", "Leading Jet Mass", 50, 0, 600)
-    m2 = TH1D("jet2m", "Subleading Jet Mass", 50, 0, 500)
-    pt1 = TH1D("jet1pt", "Leading Jet pT", 50, 0, 3000)
-    pt2 = TH1D("jet2pt", "Subleading Jet pT", 50, 0, 3000)
-    mjj = TH1D("dijetmass", "Dijet Mass", 100, 0, 10000)
-    jetm = TH3D("jetmass", "Jet Masses", 50, 0, 500, 50, 0, 500, 100, 0, 10000)
+    binsize = 15 # for dijet mass
+    dijetmassrange = 4000
 
-    # TODO: store/pull kfactor, xsec, filtering eff
-    for i, f in enumerate(iter_outfiles(sys.argv[1])):
-        tree = f.Get("aTree")
-        for evt in tree:
-            try:
-                leading, subleading, dijet, p1, p2 = masses(evt)
-                m1.Fill(leading, evt.weight)
-                m2.Fill(subleading, evt.weight)
-                mjj.Fill(dijet, evt.weight)
-                jetm.Fill(leading, subleading, dijet, evt.weight)
-                pt1.Fill(p1, evt.weight)
-                pt2.Fill(p2, evt.weight)
-            except IndexError:
-                pass
+    m1 = TH1D("jet1m", "Leading Jet Mass", 100, 0, 200)
+    m2 = TH1D("jet2m", "Subleading Jet Mass", 100, 0, 200)
+    mjj = TH1D("dijetmass", "Dijet Mass", dijetmassrange/binsize, 0, dijetmassrange)
+    jetm = TH3D("jetmass", "Jet Masses", 100, 0, 200, 100, 0, 200, dijetmassrange/binsize, 0, dijetmassrange)
+    pt1 = TH1D("jet1pt", "Leading Jet pT", 50, 0, 1500)
+    pt2 = TH1D("jet2pt", "Subleading Jet pT", 50, 0, 1500)
 
-    f = TFile("merged.root", "RECREATE")
+    # histograms for events where m1 ~ m2
+    m_avg = TH1D("jetm_avg", "Avg jetmass where m1 ~ m2", 100, 0, 200)
+    mjj_avg = TH1D("mjj_avg", "Dijet mass where m1 ~ m2", dijetmassrange/binsize, 0, dijetmassrange)
+    jetm_avg = TH2D("jetmass_avg", "Jet masses where m1 ~ m2", 100, 0, 200, dijetmassrange/binsize, 0, dijetmassrange)
+
+    for sample, sample_norm in normalization.iteritems():
+        directory = "data/user.vbaratha.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ%sW.%s_JZ%s_histOutput.root" % (sample, args.jobname, sample)
+        print "doing one dir"
+        for f in iter_outfiles(directory):
+            print "doing one file"
+            tree = f.Get("aTree")
+            for evt in tree:
+                try:
+                    _m1, _m2, _mjj, p1, p2 = masses(evt)
+                    wt = evt.weight * sample_norm
+                    m1.Fill(_m1, wt)
+                    m2.Fill(_m2, wt)
+                    mjj.Fill(_mjj, wt)
+                    jetm.Fill(_m1, _m2, _mjj, wt)
+                    pt1.Fill(p1, wt)
+                    pt2.Fill(p2, wt)
+                    if abs(_m1 - _m2) < MASS_DIFF_CUTOFF:
+                        mavg = (_m1 + _m2) / 2
+                        m_avg.Fill(mavg, wt)
+                        mjj_avg.Fill(_mjj, wt)
+                        jetm_avg.Fill(mavg, _mjj, wt)
+                except IndexError:
+                    pass
+
+    f = TFile(args.outfile, "RECREATE")
 
     m1.Write()
     m2.Write()
@@ -54,9 +84,18 @@ def main():
     jetm.Write()
     pt1.Write()
     pt2.Write()
+    m_avg.Write()
+    mjj_avg.Write()
+    jetm_avg.Write()
 
     f.Close()
     
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Merge background samples from Grid job output")
+    parser.add_argument("--jobname", type=str, required=True,
+                        help="eg Jun30_1445, part of what you called the job when it was run")
+    parser.add_argument("--outfile", type=str, default="merged.root",
+                        help="output file name")
+    args = parser.parse_args()
+    main(args)
